3dee7b4664a92eec3d19bb175ac415c122bc1ffb
Ashiness <Ashiness@DESKTOP-6BKAQ3C>
2021-01-15 23:07:30 +0300

    add comment

--- Homework2/Program.cs
+++ Homework2/Program.cs
@@ -10,7 +10,7 @@ namespace Homework2
     {
         static void Main(string[] args)
         {
-            Console.Write("заебумба");
+            Console.Write("заебумба"); //вывод сообщения
         }
     }
 }
