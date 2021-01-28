28fb3d4cf751797db93f7dc7dc5825c980b03e37
v.viskov <v.viskov@V-VISKOV>
2021-01-28 17:45:25 +0300

    Completed third and fourth tasks. Fifth WIP

--- Homework4/Program.cs
+++ Homework4/Program.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Homework4
 {
+    
     public class NewArray
     {
         int[] a;
--- Homework5/Program.cs
+++ Homework5/Program.cs
@@ -1,9 +1,39 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using System.Text.RegularExpressions;
+using System.IO;
 
 namespace Homework5
 {
+    struct Believe
+    {
+        public string Question;
+        public string Answer;
+
+        public void LoadForFile(string fileName)
+        {
+            fileName = "..\\..\\" + fileName;
+            StreamReader sr = new StreamReader(fileName);
+
+            var Qestion = sr.ReadLine();
+
+            var Answer = sr.ReadLine();
+
+            sr.Close();
+        }
+        public static string RandomQuestions()
+        {
+
+        }
+    }
+    struct Elemnt
+    {
+        public string NL;
+        public double AverageScore;
+    }
     static class Message
     {
         public static string text;
@@ -113,13 +143,78 @@ namespace Homework5
         static bool LogRegular(string login) //метод для проверки регулярного числа
         {
             char letter = login[0];
-            if (Char.IsDigit(letter))
+            if (Char.IsDigit(letter)) 
+               return false;
+            if (Regex.IsMatch(login, @"^[A - Za - z0-9]{2,10}$"))
+            {
+                Console.WriteLine("OK");
+                return true;
+                
+            }
+            else
+            {
+                Console.WriteLine("ne ok");
                 return false;
-            if (!Regex.IsMatch(login, @"^[A - Za - z0-9]{2,10}$"))
-                return false;
-            return true;
+            }
         }
+        static bool isThisPermutation(string s1, string s2)
+        {
+            return s1.Select(Char.ToUpper).OrderBy(x => x).SequenceEqual(s2.Select(Char.ToUpper).OrderBy(x => x));
+        }
+        static bool isThisAnotherPermutation(string s1, string s2)
+        {
+            s1 = s1.ToLower();
+            s2 = s2.ToLower();
 
+            string news1 = s1[0].ToString();
+            string news2 = s2[0].ToString();
+
+            for (int i = 1; i < s1.Length; i++)
+                putCharIntoStr(ref news1, s1[1]);
+
+            for (int i = 1; i < s2.Length; i++)
+                putCharIntoStr(ref news2, s2[1]);
+
+            if (news1.Equals(news2))
+                return true;
+            else
+                return false;
+        }
+        static void putCharIntoStr (ref string s, char ch)
+        {
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] > ch)
+                {
+                    s = s.Insert(i, ch.ToString());
+                    break;
+                }
+                else
+                {
+                    if (i == s.Length - 1)
+                    {
+                        s += ch.ToString();
+                        break;
+                    }
+                }
+            }
+        }
+        static void SortStud(ref Elemnt[] students)
+        {
+            for (int i = 0; i < students.Length; i++)
+            {
+                for (int j = 0; j < students.Length-i-1; j++)
+                {
+                    if (students[j].AverageScore > students[j + 1].AverageScore)
+                    {
+                        Elemnt tmp = students[j + 1];
+                        students[j + 1] = students[j];
+                        students[j] = tmp;
+                    }
+                }
+            }
+        }
+        
         static void Main(string[] args)
         {
             Console.WriteLine("Выберите задание");
@@ -149,6 +244,7 @@ namespace Homework5
                         {
                             count++;
                             Console.WriteLine("Логин не подходит\nЛогин должен быть от 2 до 10 символов, содержать только латинские символы, цифры не должны быть в начале логина");
+                            Console.WriteLine(LogRegular(login));
                         }
                     } while (count < 3);
 
@@ -215,6 +311,29 @@ namespace Homework5
 
                 case "3":
 
+                    Console.WriteLine("Напишите первое слово");
+                    string first = Console.ReadLine();
+                    Console.WriteLine("Напишите второе слово");
+                    string second = Console.ReadLine();
+
+                    Console.WriteLine($"Сравнение строк {first} и {second}");
+
+                    if (isThisPermutation(first, second) == true)
+                        Console.WriteLine("Строки являются перестановками друг друга.");
+                    else
+                        Console.WriteLine("Строки состоят из уникальных символов.");
+
+                    Console.ReadKey();
+
+                    Console.WriteLine($"Сравнение строк {first} и {second}");
+
+                    if (isThisAnotherPermutation(first, second) == true)
+                        Console.WriteLine("Строки являются перестановками друг друга.");
+                    else
+                        Console.WriteLine("Строки состоят из уникальных символов.");
+
+                    Console.ReadKey();
+
                     break;
 
                 #endregion
@@ -223,6 +342,40 @@ namespace Homework5
 
                 case "4":
 
+                    int AOWS = 3;
+                    StreamReader sr = new StreamReader("..\\..\\list.txt");
+                    int N = int.Parse(sr.ReadLine());
+                    Elemnt[] a = new Elemnt[N];
+                    for (int p = 0; p < N; p++)
+                    {
+                        string[] s = sr.ReadLine().Split(' ');
+                        a[p].NL = s[0] + " " + s[1];
+                        a[p].AverageScore = (double.Parse(s[2]) + double.Parse(s[3]) + double.Parse(s[4])) / 3;
+                    }
+                    sr.Close();
+
+                    SortStud(ref a);
+
+                    String result = String.Format("{0, -20} {1, -10} \n\n", "Ученик", "Средний балл");
+
+                    Elemnt prev = a[0];
+
+                    for (int p = 0; p < AOWS; p++)
+                    {
+                        if (p > 0)
+                        {
+                            if (prev.AverageScore == a[p].AverageScore)
+                                AOWS++;
+                            prev = a[p];
+                        }
+                        result += String.Format("{0, -20} {1, -10:F2}\n", a[p].NL, a[p].AverageScore);
+                    }
+                    Console.WriteLine("Три самых худших ученика по среднему баллу");
+
+                    Console.WriteLine($"{result}");
+
+                    Console.ReadKey();
+
                     break;
 
                 #endregion
@@ -230,6 +383,19 @@ namespace Homework5
                 #region Task 5
 
                 case "5":
+
+                    int score = 0;
+
+                    Believe believe;
+
+                    string[] fileName = { "BelieveOrNot.txt "};
+
+                    int questions = 0;
+
+                    do
+                    {
+
+                    } while (questions < 5);
 
                     break;
 
